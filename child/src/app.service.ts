fe9481140f20771da186c1eae7dcdf604d8f5adc
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-12-05 11:45:55 +1100

    record custom event

--- child/src/app.service.ts
+++ child/src/app.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     return newrelic.startSegment('getHelloService', false, () => {
+      // https://docs.newrelic.com/docs/apm/agents/nodejs-agent/api-guides/nodejs-agent-api/#increment_metric
+      newrelic.incrementMetric('API/HelloService', 1);
       return 'Hello World!';
     });
   }
